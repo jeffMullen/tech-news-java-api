bfcc25180ea24020347d5905947599a94099757d
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-02-28 14:34:22 -0500

    creating user and post models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int number = 6;
+        double decimal = 6.345;
+
+        double sum = number + decimal;
+
+        String characters = "Sherlock";
+
+        String merge = number + characters;
+
+        System.out.println(sum);
+        System.out.println(merge);
+    }
+}
---
+++ src/main/java/com/technews/model/Post.java
@@ -0,0 +1,151 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
+public class Post implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String postURL;
+    @Transient
+    private String userName;
+    @Transient
+    private int voteCount;
+    private Integer userId;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "posted_at")
+    private Date postedAt = new Date();
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_at")
+    private Date updatedAt = new Date();
+
+    @OneToMany(mappedBy = "postId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> comments;
+
+    public Post(Integer id, String title, String postURL, String userName, int voteCount, Integer userId, Date postedAt, Date updatedAt, List<Comment> comments) {
+        this.id = id;
+        this.title = title;
+        this.postURL = postURL;
+        this.userName = userName;
+        this.voteCount = voteCount;
+        this.userId = userId;
+        this.postedAt = postedAt;
+        this.updatedAt = updatedAt;
+        this.comments = comments;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPostURL() {
+        return postURL;
+    }
+
+    public void setPostURL(String postURL) {
+        this.postURL = postURL;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getPostedAt() {
+        return postedAt;
+    }
+
+    public void setPostedAt(Date postedAt) {
+        this.postedAt = postedAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post)) return false;
+        Post post = (Post) o;
+        return voteCount == post.voteCount && id.equals(post.id) && title.equals(post.title) && postURL.equals(post.postURL) && userName.equals(post.userName) && userId.equals(post.userId) && postedAt.equals(post.postedAt) && updatedAt.equals(post.updatedAt) && comments.equals(post.comments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, postURL, userName, voteCount, userId, postedAt, updatedAt, comments);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", postURL='" + postURL + '\'' +
+                ", userName='" + userName + '\'' +
+                ", voteCount=" + voteCount +
+                ", userId=" + userId +
+                ", postedAt=" + postedAt +
+                ", updatedAt=" + updatedAt +
+                ", comments=" + comments +
+                '}';
+    }
+}
